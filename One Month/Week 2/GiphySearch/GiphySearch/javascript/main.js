aa8d3144bd3f2ea2de6fcd31c85377641555b6b5
Aminul Islam Alvi <aminulialvi@gmail.com>
2020-01-18 18:42:22 +0600

    Added: pushToAPI function

--- One Month/Week 2/GiphySearch/GiphySearch/javascript/main.js
+++ One Month/Week 2/GiphySearch/GiphySearch/javascript/main.js
@@ -4,7 +4,7 @@
 document.querySelector(".js-go").addEventListener('click',function(){
 
     var input = document.querySelector("input").value;
-    pushToDOM(input);
+    pushToAPI(input);
   
   });
   
@@ -14,27 +14,36 @@ document.querySelector(".js-go").addEventListener('click',function(){
   
     // if the key ENTER is pressed...
     if(e.which === 13) {
-      pushToDOM(input);
+      pushToAPI(input);
     }
   
   });
   
   /* 2. do the data stuff with the API */
+
+ function pushToAPI(input){
+
+    var url = "http://api.giphy.com/v1/gifs/search?q=" +input+ "&api_key=dc6zaTOxFJmzC"
+    console.log(url);
   
-  var url = "http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC";
+    // AJAX Request
+    var GiphyAJAXCall = new XMLHttpRequest();
+    GiphyAJAXCall.open( 'GET', url );
+    GiphyAJAXCall.send();
+    
+    GiphyAJAXCall.addEventListener('load',function(e){
+    
+      var data = e.target.response;
+      pushToDOM(data);
+    
+    });
+
+ }
+    
+    
+   
   
-  // AJAX Request
-  var GiphyAJAXCall = new XMLHttpRequest();
-  GiphyAJAXCall.open( 'GET', url );
-  GiphyAJAXCall.send();
-  
-  GiphyAJAXCall.addEventListener('load',function(e){
-  
-    var data = e.target.response;
-    pushToDOM(data);
-  
-  });
-  
+
   
   
   
@@ -42,13 +51,21 @@ document.querySelector(".js-go").addEventListener('click',function(){
   
   
   function pushToDOM(input) {
+
+    var container = document.querySelector(".js-container");
   
     var response = JSON.parse(input);
   
-    var imageURL = response.data[0].images.fixed_height.url;
-    console.log(imageURL);
+    var imageUrls = response.data;
   
-    var container = document.querySelector(".js-container");
-    container.innerHTML = "<img src=\"" + imageURL + "\">";
+    imageUrls.forEach(function(image){
+  
+      var src = image.images.fixed_height.url;
+      console.log(src);
+  
+     
+      container.innerHTML += "<img src=\"" + src + "\" class=\"container-image\">";
+  
+    });
   
   }
